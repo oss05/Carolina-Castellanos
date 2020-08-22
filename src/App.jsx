55f511287f41c3147c732c388d81864b33e7e45f
Bryan <bryanblack1999@gmail.com>
2020-08-21 20:07:39 -0500

    preparando entorno de desarrollo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BroserRouter , Switch , Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return <p>hola mundo</p>
+}
+
+export default App;
